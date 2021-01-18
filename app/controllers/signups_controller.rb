034ffed9041b850f91e35dd3c9fc736af9dffabd
“nahuel” <n.gorosito@yahoo.com>
2021-01-18 15:40:49 -0500

    Make thanks page

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -4,4 +4,18 @@ class SignupsController < ApplicationController
     @signup = Signup.new
   end
 
+  def create
+    @signup = Signup.new(signup_params)
+    if @signup.save
+      redirect_to :thanks
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def signup_params
+    params.require(:signup).permit(:firstname, :email)
+  end
 end
